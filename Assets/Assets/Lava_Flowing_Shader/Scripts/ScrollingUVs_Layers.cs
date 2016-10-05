36d859eff29f8307176e5a233dd47967139cdc19
Benoit Boyer <contact@benoitbm.fr>
2016-10-05 20:13:26 +0200

    Rajout de textures pour le tuto
    Parce que les ambiances type matrices pour les tutos, c'est cool.

--- Assets/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
+++ Assets/Assets/Lava_Flowing_Shader/Scripts/ScrollingUVs_Layers.cs
@@ -5,7 +5,7 @@ public class ScrollingUVs_Layers : MonoBehaviour
 {
 	//public int materialIndex = 0;
 	public Vector2 uvAnimationRate = new Vector2( 1.0f, 0.0f );
-	public string textureName = "_MainTex";
+	/*public*/ string textureName = "_MainTex";
 	
 	Vector2 uvOffset = Vector2.zero;
 	
