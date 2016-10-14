5f47310333ab146f504e99cea764e7eeeb8e0310
Benoit Boyer <contact@benoitbm.fr>
2016-10-14 15:20:19 +0200

    Light fix
    When you try an emitting light system, but it's better to use integrated
    lights for the level ¯\_(ツ)_/¯

---
+++ Assets/Scripts/EmittingLight.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//Script à la base pour utiliser les matériaux "Emetteurs" (pour la lumière) et faire un niveau basé que sur ça. J'ai du changer le système et plutôt utiliser des lumières intégrés, ce qui rend l'effet similaire. 
+public class EmittingLight : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DynamicGI.UpdateMaterials(gameObject.GetComponent<Renderer>());
+        DynamicGI.UpdateEnvironment();
+	}
+}
